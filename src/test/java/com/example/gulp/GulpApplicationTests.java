df4510a4578c1bb0dc30121efc893af90fed7540
chloe kang <kjhjjlove@gmail.com>
2023-01-05 23:07:26 +0900

    init

---
+++ src/test/java/com/example/gulp/GulpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gulp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
